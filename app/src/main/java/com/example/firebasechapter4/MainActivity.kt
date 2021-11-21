ba3fdcc5ace5d5293f823e154268b7ff2911e14e
reina.tanaka <ponzusoup.223@gmail.com>
2021-11-22 01:57:43 +0900

    Initial commit

---
+++ app/src/main/java/com/example/firebasechapter4/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.firebasechapter4
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import com.example.firebasechapter4.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var dataText: TextView? = null
+    private var people: CollectionReference? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        dataText = findViewById(R.id.dataText);
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        val db = Firebase.firestore
+        people = db.collection("people")
+
+        people!!.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                var result = ""
+                for (document in task.result!!) {
+                    val data = document.data
+                    result += (data["name"].toString() + " ["
+                            + data["mail"].toString() + ":"
+                            + data["age"].toString() + "]\n")
+                }
+                dataText!!.setText(result)
+            } else {
+                dataText!!.setText("can't load data...")
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
